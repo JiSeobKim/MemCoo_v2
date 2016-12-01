0328b374f0364851c3a08a05b468e9bc34fa5044
Developer-MINO <mino.kim.1st@gmail.com>
2016-12-02 03:07:07 +0900

    Initial Commit

---
+++ CouponUI/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CouponUI
+//
+//  Created by mino on 2016. 12. 2..
+//  Copyright © 2016년 mino. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

b91d15a49fc79d96555cf81628be217cec1ce774
mino <mino.kim.1st@gmail.com>
2017-01-03 16:03:36 +0900

    사용 완료 버튼 기능 추가

--- CouponUI/CouponDetailViewController.swift
+++ CouponUI/CouponDetailViewController.swift
@@ -13,9 +13,7 @@ class CouponDetailViewController: UIViewController, UINavigationControllerDelega
     @IBOutlet weak var barcode: UILabel!
     @IBOutlet weak var expireDate: UILabel!
     @IBOutlet weak var category: UILabel!
-    @IBAction func finishButton(_ sender: Any) {
-        //self.navigationItem.rightBarButtonItem
-    }
+    @IBOutlet weak var finishButtonOutlet: UIButton!
     
     //쿠폰뷰콘트롤러에서 받는 couponToDetail과 쿠폰애드뷰콘트롤러에 전달해주는 couponToEdit이 있다.
     var couponToDetail: Coupon?
@@ -23,6 +21,23 @@ class CouponDetailViewController: UIViewController, UINavigationControllerDelega
     
     //밝기 조절용
     var bright : CGFloat?
+    
+    @IBAction func finishButton(_ sender: Any) {
+        let alert = UIAlertController(title: "사용 완료하시겠습니까?", message: "사용 완료 후 되돌릴 수 없습니다!", preferredStyle: .alert)
+        let finish = UIAlertAction(title: "사용 완료", style: .destructive) {
+            (_) in
+            if self.couponToDetail != nil {
+                self.couponToDetail?.isUsed = true
+                ad.saveContext()
+            }
+            _ = self.navigationController?.popToRootViewController(animated: true)
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(finish)
+        alert.addAction(cancel)
+        self.present(alert, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +45,14 @@ class CouponDetailViewController: UIViewController, UINavigationControllerDelega
         if couponToDetail != nil {
             loadCouponData()
             self.navigationItem.title = titleName
+            if couponToDetail?.isUsed == true {
+                self.navigationItem.rightBarButtonItem?.isEnabled = false
+                barcodeImg.isHidden = true
+                barcode.text = "사용 완료"
+                expireDate.isHidden = true
+                category.isHidden = true
+                finishButtonOutlet.isHidden = true
+            }
         }
         
         if let topItem = self.navigationController?.navigationBar.topItem {
@@ -43,7 +66,6 @@ class CouponDetailViewController: UIViewController, UINavigationControllerDelega
     
     //아이템 데이타를 로드하는 펑션
     func loadCouponData() {
-        
         if let coupon = couponToDetail {
             barcode.text = coupon.barcode
             barcodeImg.image = generateBarcodeFromString(string: coupon.barcode)
@@ -69,6 +91,14 @@ class CouponDetailViewController: UIViewController, UINavigationControllerDelega
     override func viewWillAppear(_ animated: Bool) {
         if couponToDetail != nil {
             loadCouponData()
+            if couponToDetail?.isUsed == true {
+                self.navigationItem.rightBarButtonItem?.isEnabled = false
+                barcodeImg.isHidden = true
+                barcode.text = "사용 완료"
+                expireDate.isHidden = true
+                category.isHidden = true
+                finishButtonOutlet.isHidden = true
+            }
         }
     }
     /*

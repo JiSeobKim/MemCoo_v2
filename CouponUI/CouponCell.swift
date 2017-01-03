fcc37d2d7f2cd920f1be34375d7a242b3105485f
mino <mino.kim.1st@gmail.com>
2017-01-03 13:09:03 +0900

    1. 클립보드 파싱 시 키보드 자동으로 뜨지 않도록 수정. 2. OCR 파싱 시 메모에 표시되도록 수정.

--- CouponUI/CouponCell.swift
+++ CouponUI/CouponCell.swift
@@ -9,24 +9,16 @@
 import UIKit
 
 class CouponCell: UITableViewCell {
-    
     @IBOutlet weak var title: UILabel!
-    
     @IBOutlet weak var category: UILabel!
-
     @IBOutlet weak var expireDate: UILabel!
-    
     @IBOutlet weak var dday: UILabel!
-    
     @IBOutlet weak var logo: UIImageView!
-    
     @IBOutlet weak var barcodeImg: UIImageView!
-    
     func configureCell(item: Coupon) {
-        
         title.text = item.title
         //category는 to many관계이므로 처리하기 어렵다.
-        category.text = "보류다."
+        category.text = "카테고리"
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         expireDate.text = dateFormatter.string(from: item.expireDate as! Date)
@@ -35,7 +27,6 @@ class CouponCell: UITableViewCell {
         barcodeImg.image = generateBarcodeFromString(string: item.barcode)
         
         dday.text = daysBetweenDates(startDate: Date(), endDate: item.expireDate as! Date)
-
     }
     
     override func awakeFromNib() {
@@ -45,8 +36,6 @@ class CouponCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
-
 }

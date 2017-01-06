4b3187cc02df78d8ee498caa7e4185aba28e3b94
mino <mino.kim.1st@gmail.com>
2017-01-07 02:28:50 +0900

    설정 테이블 뷰 컨트롤러, 테이블 뷰 셀 파일 추가

---
+++ SettingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsTableViewCell.swift
+//  MemebershipCoupon
+//
+//  Created by mino on 2017. 1. 7..
+//  Copyright © 2017년 mino. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

3056f29d2dffebb3d3f9879a64c0fb6fe7f932a2
JiseobKim <jiseob812@gmail.com>
2017-03-31 22:32:59 +0900

    1. 네비바 폰트 조절 2. 휴지통 색 수정 3. 로고 셀렉트 뷰 테두리 수정

--- CouponUI/LogoCorner.swift
+++ CouponUI/LogoCorner.swift
@@ -22,29 +22,39 @@ extension UIImageView {
             logoCorner = newValue
                 if logoCorner {
                     
-                    self.layer.borderColor = UIColor(red: 233/255.0, green: 233/255.0, blue: 233/255.0, alpha: 1.0).cgColor
-                    self.layer.masksToBounds = true
-                    if UIScreen.main.bounds.width == 320 {
-                        self.layer.borderWidth = 1.5
-                        if self.frame.width > 100 {
-                            self.layer.cornerRadius = 20
-                        } else {
-                            self.layer.cornerRadius = 10
-                        }
-
-                    } else {
-                        self.layer.borderWidth = 2
-                        if self.frame.width > 100 {
-                            self.layer.cornerRadius = 25
-                        } else {
-                            self.layer.cornerRadius = 15
-                        }
-                        
-                    }
+//                    viewLogoShow.layer.borderWidth = 1
+//                    viewLogoShow.layer.borderColor = UIColor(netHex: 0xF66623,alpha: 0.78).cgColor
+//                    viewLogoShow.layer.cornerRadius = (viewLogoShow.frame.width/2)
+//                    viewLogoShow.layer.masksToBounds = true
                     
-                } else {
-                    self.layer.borderWidth = 0
-                    self.layer.cornerRadius = 0
+                    self.layer.borderWidth = 1
+                    self.layer.borderColor = UIColor(netHex: 0xF66623,alpha: 0.78).cgColor
+                    self.layer.cornerRadius = (self.frame.width/2)
+                    self.layer.masksToBounds = true
+                    
+//                    self.layer.borderColor = UIColor(red: 233/255.0, green: 233/255.0, blue: 233/255.0, alpha: 1.0).cgColor
+//                    self.layer.masksToBounds = true
+//                    if UIScreen.main.bounds.width == 320 {
+//                        self.layer.borderWidth = 1.5
+//                        if self.frame.width > 100 {
+//                            self.layer.cornerRadius = 20
+//                        } else {
+//                            self.layer.cornerRadius = 10
+//                        }
+//
+//                    } else {
+//                        self.layer.borderWidth = 2
+//                        if self.frame.width > 100 {
+//                            self.layer.cornerRadius = 25
+//                        } else {
+//                            self.layer.cornerRadius = 15
+//                        }
+//                        
+//                    }
+//                    
+//                } else {
+//                    self.layer.borderWidth = 0
+//                    self.layer.cornerRadius = 0
             }
         }
     }
